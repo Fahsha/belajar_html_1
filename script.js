1aff58fa5a427355735efeeda7ddcac6b811c0c4
Fahsha <muhammadfakhruroji33@gmail.com>
2023-06-01 21:17:43 +0700

    ini adalah commit pertama

---
+++ script.js
@@ -0,0 +1,6 @@
+// This function will toggle the login form when the "Login" button is clicked.
+function toggleLogin() {
+    var loginForm = document.getElementById("login-form");
+    loginForm.style.display = (loginForm.style.display == "none") ? "block" : "none";
+  }
+  
